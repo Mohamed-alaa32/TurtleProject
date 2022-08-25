e7513d69179f72c2430aebc6092de2ad293713d1
Mohamed-alaa32 <111683866+Mohamed-alaa32@users.noreply.github.com>
2022-08-25 20:09:53 +0200

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed/TurtleProject/src
+# Build directory: /home/mohamed/TurtleProject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtle_pkg")
---
+++ turtle_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamed/TurtleProject/src/turtle_pkg
+# Build directory: /home/mohamed/TurtleProject/build/turtle_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
